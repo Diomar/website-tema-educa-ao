3dc2100606e5a6274ea44df4f90c0621891a89a9
Diomar <diomarpr@gmail.com>
2022-05-07 14:06:28 -0300

    section faqs css add.

--- main.js
+++ main.js
@@ -8,3 +8,19 @@ window.addEventListener('scroll', () => {
 //     document.querySelector('header').classList.toggle('window-scroll', window.scrollY > 0)
 // })
 
+// Mostrar/Esconder perguntas frequentes
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //Trocar o ícone quando abrir o card
+        const icon = faq.querySelector('.faq__icon i');
+        if (icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus";
+        } else {
+            icon.className = "uil uil-plus";
+        }
+
+    })
+})
