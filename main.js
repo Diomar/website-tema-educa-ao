f5a260eb06d0b002f2ab1d62348436e32f9dedb4
Diomar <diomarpr@gmail.com>
2022-05-07 19:48:35 -0300

    menu dropdrow toggle em js

--- main.js
+++ main.js
@@ -19,8 +19,29 @@ faqs.forEach(faq => {
         if (icon.className === 'uil uil-plus') {
             icon.className = "uil uil-minus";
         } else {
-            icon.className = "uil uil-plus";
+            icon.className = 'uil uil-plus';
         }
 
     })
 })
+
+
+// Mostrar/ Esconder MENU DROP DOW
+const menu = document.querySelector('.nav__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+// Fechar o menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none"
+    menuBtn.style.display = "inline-block"
+}
+
+closeBtn.addEventListener('click', closeNav );
